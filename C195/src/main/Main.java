3c41c46968d27d49af580f2acae053a93f2ba821
Greg Westmoreland <gwestmor@us.ibm.com>
2021-05-10 00:11:47 -0500

    SQL Injection and INSERT INTO Using Scanner Object and User Input

--- C195/src/main/Main.java
+++ C195/src/main/Main.java
@@ -17,6 +17,7 @@ import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main extends Application {
 
@@ -63,13 +64,85 @@ public class Main extends Application {
         Statement statement = DBQuery.getStatement();
 
         /**
-         * Create a Raw SQL INSERT statement for the countries table. Inserting France country record.
+         * This demonstrates a SQL Injection Attack utilizing the scanner object allowing code to be entered.
+         * Entering a 1=1 will cause all records to show instead of just the records for the country the user enters.
+         * This block of code is commented out due to it preventing the user interface from loading.
          */
-        //String insertStatement = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Updated_By) VALUES('France', '2021-05-09 15:45:09', 'admin', 'admin')";
+        /**
+        String country;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a country: ");
+        country = keyboard.nextLine();
+        String selectStatement3 = "SELECT * FROM countries WHERE " + country;
+        // Try Catch Block Exception Handling
+        try {
+            statement.execute(selectStatement3);
+            ResultSet rs = statement.getResultSet();
+            // Forward Scroll ResultSet
+            while (rs.next()) {
+                int Country_ID2 = rs.getInt("Country_ID");
+                String Country2 = rs.getString("Country");
+                LocalDate Create_Date2 = rs.getDate("Create_Date").toLocalDate();
+                LocalTime Create_Time2 = rs.getTime("Create_Date").toLocalTime();
+                String Created_By2 = rs.getString("Created_By");
+                LocalDateTime Last_Update_2 = rs.getTimestamp("Last_Update").toLocalDateTime();
+                String Last_Updated_By2 = rs.getString("Last_Updated_By");
+                // Display Result Set
+                System.out.println(Country_ID2 + " | " + Country2 + " | " + Create_Date2 + " | " + Create_Time2 + " | " +
+                        Created_By2 + " | " + Last_Update_2 + " | " + Last_Updated_By2);
+            }
+        } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }*/
+
+        /**
+         * This demonstrates a SQL Insert Statement using the Scanner Object to enter a country name to create a new
+         * record in the countries table. We are only creating the Country, Create_Date, Created_By, and
+         * Last_Updated_By values. This insert statement has been commented out.
+         */
+        /**
+         String Country, Create_Date, Created_By, Last_Updated_By;
+         Scanner keyboard = new Scanner(System.in);
+         System.out.print("Enter a country: ");
+         Country = keyboard.nextLine();
+         Create_Date = "2020-05-09 23:18:46";
+         Created_By = "admin";
+         Last_Updated_By = "admin";
+
+         // Replace any entries that contain a single quote when users enters a country like: 'country'
+         if(Country.contains("'"))
+             Country = Country.replace("'", "\\'");
+
+         String insertStatement2 = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Updated_By)" +
+                 "VALUES( " +
+                 "'" + Country + "'," +
+                 "'" + Create_Date + "'," +
+                 "'" + Created_By + "'," +
+                 "'" + Last_Updated_By + "'" +
+                 ")";
+         // Try Catch Block Exception Handling
+         try {
+            statement.execute(insertStatement2);
+             if(statement.getUpdateCount() > 0)
+                 System.out.println(statement.getUpdateCount() + " row(s) affected.");
+             else
+                 System.out.println("No changes were made.");
+         } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
+         }
+         */
+
+        /**
+         * Create a Raw SQL INSERT statement for the countries table. Inserting France country record.
+         * This line of code is commented out.
+         */
+        //String insertStatement3 = "INSERT INTO countries(Country, Create_Date, Created_By, Last_Updated_By) VALUES('France', '2021-05-09 15:45:09', 'admin', 'admin')";
 
         /**
          * Create a SQL INSERT statement for the countries table using variables . Inserting France country record.
+         * This comment is commented out.
          */
+
         String Country = "France";
         String Create_Date = "2021-05-09 15:45:09";
         String Created_By = "admin";
@@ -90,6 +163,7 @@ public class Main extends Application {
         else
             System.out.println("No changes were made and no rows were updated.");
 
+
         /**
          * Create a Raw SQL UPDATE statement for the countries table. Updating country record to be changed to Japan.
          */
@@ -103,7 +177,7 @@ public class Main extends Application {
         /**
          * Create a Raw SQL DELETE statement for the countries table. Deleting country record.
          */
-        String deleteStatement = "DELETE FROM countries WHERE Country_ID = 100;";
+        String deleteStatement = "DELETE FROM countries WHERE Country_ID = 100000000;";
         statement.execute(deleteStatement);
         if(statement.getUpdateCount() > 0)
             System.out.println(statement.getUpdateCount() + " record updated.");
@@ -114,20 +188,25 @@ public class Main extends Application {
          * ResultSet Object from the countries table to store all records in selectStatement2 variable 
          */
         String selectStatement2 = "SELECT * FROM countries;";
-        statement.execute(selectStatement2);
-        ResultSet rs = statement.getResultSet();
-        // Forward Scroll ResultSet
-        while(rs.next()) {
-            int Country_ID2 = rs.getInt("Country_ID");
-            String Country2 = rs.getString("Country");
-            LocalDate Create_Date2 = rs.getDate("Create_Date").toLocalDate();
-            LocalTime Create_Time2 = rs.getTime("Create_Date").toLocalTime();
-            String Created_By2 = rs.getString("Created_By");
-            LocalDateTime Last_Update_2 = rs.getTimestamp("Last_Update").toLocalDateTime();
-            String Last_Updated_By2 = rs.getString("Last_Updated_By");
-            // Display Result Set
-            System.out.println(Country_ID2 + " | " + Country2 + " | " + Create_Date2 + " | " + Create_Time2 + " | " +
-                    Created_By2 + " | " + Last_Update_2 + " | " + Last_Updated_By2);
+        // Try Catch Block Exception Handling
+        try {
+            statement.execute(selectStatement2);
+            ResultSet rs = statement.getResultSet();
+            // Forward Scroll ResultSet
+            while (rs.next()) {
+                int Country_ID2 = rs.getInt("Country_ID");
+                String Country2 = rs.getString("Country");
+                LocalDate Create_Date2 = rs.getDate("Create_Date").toLocalDate();
+                LocalTime Create_Time2 = rs.getTime("Create_Date").toLocalTime();
+                String Created_By2 = rs.getString("Created_By");
+                LocalDateTime Last_Update_2 = rs.getTimestamp("Last_Update").toLocalDateTime();
+                String Last_Updated_By2 = rs.getString("Last_Updated_By");
+                // Display Result Set
+                System.out.println(Country_ID2 + " | " + Country2 + " | " + Create_Date2 + " | " + Create_Time2 + " | " +
+                        Created_By2 + " | " + Last_Update_2 + " | " + Last_Updated_By2);
+            }
+        } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
         /**
