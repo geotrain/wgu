9a60af88e2ea536f96a2ad1d7cc41cef8ebb2154
Greg Westmoreland <gwestmor@us.ibm.com>
2021-12-30 20:17:21 -0600

    Created Reports and Search UI Pages

---
+++ C868/Conerstone Community College Student Register - Android App/app/src/main/java/android/reserver/C868_greg_westmoreland/All/UI/Main/Main_Activity_Reports_Page.java
@@ -0,0 +1,8 @@
+package android.reserver.C868_greg_westmoreland.All.UI.Main;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_Activity_Reports_Page extends AppCompatActivity {
+}
