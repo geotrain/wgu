f6fb8bbf0249f98655d68d17cb2bb0c402305afb
Greg Westmoreland <gwestmor@us.ibm.com>
2025-01-21 21:04:29 -0600

    Clean up input code

--- 100_Days_Of_Coding_Python/Band_Name_Generator.py
+++ 100_Days_Of_Coding_Python/Band_Name_Generator.py
@@ -1,6 +1,6 @@
+# This Program takes two inputs from the user in the forms of a string and then concatenates them in the final output
+# as a suggestion for a name of a band by asking two questions.
 print("Welcome to the Band Name Generator.")
-print("What's the name of the city you grew up in?")
-city = input()
-print("What's your pet's name?")
-pet_name = input()
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
 print("Your band name could be " + city + " " + pet_name)
